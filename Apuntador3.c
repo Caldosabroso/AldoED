ffe7df4682b6e3a9f1c1d4d6c8b480766d644e4f
Caldosabroso <20160863@itoaxaca.edu.mx>
2026-04-20 08:38:21 -0600

    Add files via upload
    Código para iniciar con el tema de Tipo de  Datos Abstractos, y una sucesión de listas, donde se puede acceder a ellas por medio de un solo apuntador.

---
+++ Apuntador3.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Dato{
+    int d;
+    struct Dato *ptrsig;
+};
+int menu(void){
+    printf("Indique la opcion siguiente:\n");
+    printf("[1]Crear dato\n[2]Mostrar\n[3]Liberar\n[4]Salir\n");
+    int opcion;
+    scanf("%d",&opcion);
+    return opcion;}
+
+struct Dato* creardato(){
+    struct Dato *temp = malloc(sizeof(struct Dato));
+
+    if(temp == NULL){
+        printf("Error de memoria\n");
+        return NULL;
+    }
+
+    printf("Ingresa el valor: ");
+    scanf("%d", &temp->d);
+
+    temp->ptrsig = NULL;
+
+    return temp;
+}
+void mostrardato(struct Dato *ptr){
+    while(ptr != NULL){
+        printf("%d -> ", ptr->d);
+        ptr = ptr->ptrsig;
+    }
+    printf("NULL No se ha creado\n");
+}
+void liberardato(struct Dato *ptr){
+    struct Dato *temp;
+
+    while(ptr != NULL){
+        temp = ptr;
+        ptr = ptr->ptrsig;
+        free(temp);
+    }
+}
+
+int main(void){
+    struct Dato *ptr=NULL;
+    int opcion;
+    do{
+        opcion=menu();
+        switch(opcion){
+       case 1:
+            if(ptr == NULL){
+            ptr = creardato();
+            } else {
+            struct Dato *nuevo = creardato();
+            nuevo->ptrsig = ptr;
+            ptr = nuevo;
+            }
+        break;
+
+        case 2:
+            mostrardato(ptr);
+            break;
+        case 3:
+            liberardato(ptr);
+        break;
+
+        default:
+            printf("Opcion invalida\n");
+        }
+    }while(opcion!=4);
+    return 0;
+}
