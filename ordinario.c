37cc6bc9a574241999c91dfba20556209d97b72e
Caldosabroso <20160863@itoaxaca.edu.mx>
2026-06-16 07:13:53 -0600

    Update ordinario.c
    primer menu basico

--- ordinario.c
+++ ordinario.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct persona{
+struct Alumno;
+struct Persona{
     char *nombre;//fgets y luego calcular con malloc
     int edad;//malloc
     char genero;//[1]Masculino,[2]Femenino
     char fn[8];//dd/mm/aa
-    struct *persona;
-    struct *alumno;
-}
-struct alumno{
+    struct persona *sigPersona;
+    struct struct *ptrAlumno;
+};
+struct Alumno{
     char matricula[10];
     char carrera[5];//IDDSI,LCA,LAT,LB,LF.
     int semestre;//1,2,3,4,5,6,7,8,9,10
     char correo[23];//
     float calificacion[5][5];//3 parciales y 1 ordinario,5 promedios, 5 materias
-}
+};
 int main(){
 
     do{
-        printf("--------MENU--------\n[1] Agregar persona\n[2] Eliminar persona\n [3]")
+        printf("--------MENU--------\n[1] Agregar persona\n[2] Eliminar persona\n[3] Mostrar datos\n[4] Sin definir\n[5] Terminar programa\n ");
 
     }while(opcion!=5)
 
